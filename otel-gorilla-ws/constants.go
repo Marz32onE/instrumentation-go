e87bcb3ace0be2caf1e1bd1d3c3305e756f4f771
marz32one <death0032@gmail.com>
2026-04-13 21:12:32 +0800

    update otel-gorilla-ws interfaces

--- otel-gorilla-ws/constants.go
+++ otel-gorilla-ws/constants.go
@@ -2,6 +2,9 @@ package otelgorillaws
 
 import "github.com/gorilla/websocket"
 
+// CloseError re-exports gorilla/websocket close error type.
+type CloseError = websocket.CloseError
+
 // Re-export gorilla/websocket message type constants so users can import only this package.
 const (
 	TextMessage   = websocket.TextMessage
@@ -9,4 +12,22 @@ const (
 	CloseMessage  = websocket.CloseMessage
 	PingMessage   = websocket.PingMessage
 	PongMessage   = websocket.PongMessage
+)
+
+// Re-export gorilla/websocket close code constants for close/error handling.
+const (
+	CloseNormalClosure           = websocket.CloseNormalClosure
+	CloseGoingAway               = websocket.CloseGoingAway
+	CloseProtocolError           = websocket.CloseProtocolError
+	CloseUnsupportedData         = websocket.CloseUnsupportedData
+	CloseNoStatusReceived        = websocket.CloseNoStatusReceived
+	CloseAbnormalClosure         = websocket.CloseAbnormalClosure
+	CloseInvalidFramePayloadData = websocket.CloseInvalidFramePayloadData
+	ClosePolicyViolation         = websocket.ClosePolicyViolation
+	CloseMessageTooBig           = websocket.CloseMessageTooBig
+	CloseMandatoryExtension      = websocket.CloseMandatoryExtension
+	CloseInternalServerErr       = websocket.CloseInternalServerErr
+	CloseServiceRestart          = websocket.CloseServiceRestart
+	CloseTryAgainLater           = websocket.CloseTryAgainLater
+	CloseTLSHandshake            = websocket.CloseTLSHandshake
 )
